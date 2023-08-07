61969cfccfbd98caba1981662ad7fdf4e43ce993
luiz-chiq <chiquetanoluiz@gmail.com>
2023-08-07 08:15:18 -0300

    att wecomp

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,7 @@
 import "../style/components/Carousel.css"
 import "../style/components/Button.css"
 import { useState, useEffect } from "react"
+import Arrow from "./Arrow.js"
 
 function Carousel({ children }){
     const [ index, setIndex ] = useState(0)
@@ -121,8 +122,13 @@ function Carousel({ children }){
                     ))
                 }</div>
             </div>
+            
+            <Arrow action={() => moveBackward()} direction="backward" clickability={isAllowedToMoveBackward()}/>
+            <Arrow action={() => moveForward()} direction="forward" clickability={isAllowedToMoveForward()}/>
 
-
+            
+        
+        {/* 
             { isAllowedToMoveBackward() ? <div className="control-button previous">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
                             fill="currentColor" className="bi bi-chevron-compact-left clickable" viewBox="0 0 16 16"
@@ -151,8 +157,8 @@ function Carousel({ children }){
                         <path d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
                     </svg>
                 </div>
-
-            }
+            } 
+            */}
         </div>
     );
 }
